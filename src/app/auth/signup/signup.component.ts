0798bede6adf91e367d782a7335e309ae9a91ba9
treyvon <Eugenewhitaker2022@gmail.com>
2024-04-09 22:23:27 -0400

    fix(forum-client): update signup ts file

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
+  signupForm: FormGroup | null = null;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      password: new FormControl(null)
+    });
+  }
 }
