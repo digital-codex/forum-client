1dbee268e1c48e1b8e3403b218d6b4506f9e1891
treyvon <Eugenewhitaker2022@gmail.com>
2024-04-18 21:26:03 -0400

    fix(forum-client): added auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule} from "@angular/forms";
+import {SignupComponent} from "./signup/signup.component";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule
